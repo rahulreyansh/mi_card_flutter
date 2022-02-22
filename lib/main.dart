df05d896b629e48ba3e4e5e51cab245a83c96caa
Rahul Kumar <83118953+rahulreyansh@users.noreply.github.com>
2022-02-22 13:34:19 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,73 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('image/cricleAva.jpg'),
+              ),
+              Text(
+                'Rahul',
+                style: TextStyle(
+                  fontSize: 38.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                    color: Colors.teal.shade200, height: 2.0, thickness: 2.0),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text(
+                    '+91 9361421855',
+                    style: TextStyle(color: Colors.teal, fontSize: 18.0),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.mail, color: Colors.teal),
+                  title: Text(
+                    'rahulreyansh798@gmail.com',
+                    style: TextStyle(color: Colors.teal, fontSize: 18.0),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
